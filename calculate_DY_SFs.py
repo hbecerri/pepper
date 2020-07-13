d7396cdf055e4d6f6149e9186d9b88079b36fee8
Dominic Stafford <dominic.william.stafford@cern.ch>
2020-07-13 18:27:08 +0200

    Overhaul calculate_DY_SFs.py: switch to AN2016_417 formula (applying all cuts and using 0b region to improve SFs), implement MET binning, and calculate errors

--- calculate_DY_SFs.py
+++ calculate_DY_SFs.py
@@ -7,8 +7,14 @@ import parsl
 import json
 import logging
 from argparse import ArgumentParser
+import os
+
+import sympy
 
 import pepper
+
+
+MET_bins = ["0_to_40", "40_to_70", "70_to_100", "100_to_150", "150_to_inf"]
 
 logger = logging.getLogger(__name__)
 
@@ -40,6 +46,10 @@ class DYprocessor(pepper.Processor):
         return self._accumulator
 
     def process_selection(self, selector, dsname, is_mc, filler):
+        if dsname.startswith("TTTo"):
+            selector.set_column(self.gentop, "gent_lc")
+            if self.topptweighter is not None:
+                self.do_top_pt_reweighting(selector)
         if self.config["compute_systematics"] and is_mc:
             self.add_generator_uncertainies(dsname, selector)
         if is_mc:
@@ -71,6 +81,7 @@ class DYprocessor(pepper.Processor):
         selector.add_cut(self.channel_trigger_matching, "Chn. trig. match")
         selector.add_cut(self.lep_pt_requirement, "Req lep pT")
         selector.add_cut(self.good_mll, "M_ll")
+        selector.set_column(self.z_window, "Z window")
 
         selector.set_column(self.build_jet_column, "Jet")
         selector.set_column(partial(self.build_met_column,
@@ -86,50 +97,188 @@ class DYprocessor(pepper.Processor):
         m_min = self.config["z_boson_window_start"]
         m_max = self.config["z_boson_window_end"]
         Z_window = (mll >= m_min) & (mll <= m_max)
+        MET = selector.final["MET"].pt.flatten()
+        at_least_1btag = self.btag_cut(False, selector.final)
+
         output = filler.output
+        channel = {"ee": selector.final["is_ee"],
+                   "em": selector.final["is_em"],
+                   "mm": selector.final["is_mm"]}
+        Z_w = {"in": Z_window, "out": ~Z_window}
+        btags = {"0b": ~at_least_1btag, "1b": at_least_1btag}
+        MET_bins = {"0_to_40": (MET < 40),
+                    "40_to_70": (MET > 40) & (MET < 70),
+                    "70_to_100": (MET > 70) & (MET < 100),
+                    "100_to_150": (MET > 100) & (MET < 150),
+                    "150_to_inf": (MET > 150)}
+        selector.set_column(self.met_0_to_40, "0_to_40",
+                            no_callback=True)
+        selector.set_column(self.met_40_to_70, "40_to_70",
+                            no_callback=True)
+        selector.set_column(self.met_70_to_100, "70_to_100",
+                            no_callback=True)
+        selector.set_column(self.met_100_to_150, "100_to_150",
+                            no_callback=True)
+        selector.set_column(self.met_150_to_inf, "150_to_inf",
+                            no_callback=True)
         if dsname.startswith("DY"):
             weights = selector.final_systematics["weight"].flatten()
-            self.fill_dy_nums(output,
-                              weights[selector.final["is_ee"] & Z_window],
-                              "Zee_in", dsname)
-            self.fill_dy_nums(output,
-                              weights[selector.final["is_mm"] & Z_window],
-                              "Zmm_in", dsname)
-            self.fill_dy_nums(output,
-                              weights[selector.final["is_ee"] & ~Z_window],
-                              "Zee_out", dsname)
-            self.fill_dy_nums(output,
-                              weights[selector.final["is_mm"] & ~Z_window],
-                              "Zmm_out", dsname)
+            for ch in channel.items():
+                for Zw in Z_w.items():
+                    for btag in btags.items():
+                        for MET_bin in MET_bins.items():
+                            self.fill_dy_nums(output, weights, ch, Zw, btag,
+                                              MET_bin, dsname)
         elif not is_mc:
-            self.fill_dy_nums(output,
-                              np.ones((selector.final["is_ee"]
-                                       & Z_window).sum()),
-                              "Nee_in", dsname, True)
-            self.fill_dy_nums(output,
-                              np.ones((selector.final["is_em"]
-                                       & Z_window).sum()),
-                              "Nem_in", dsname, True)
-            self.fill_dy_nums(output,
-                              np.ones((selector.final["is_mm"]
-                                       & Z_window).sum()),
-                              "Nmm_in", dsname, True)
-
+            for ch in channel.items():
+                for Zw in Z_w.items():
+                    for btag in btags.items():
+                        for MET_bin in MET_bins.items():
+                            self.fill_dy_nums(output,
+                                              np.ones(selector.final.size),
+                                              ch, Zw, btag, MET_bin, dsname,
+                                              True)
+        selector.set_column(partial(self.btag_cut, False), "At least 1 btag")
         logger.debug("Selection done")
 
-    def fill_dy_nums(self, output, weights, name, dsname, data=False):
+    def met_0_to_40(self, data):
+        ret_arr = np.full(data.size, False)
+        MET = data["MET"].pt.flatten()
+        ret_arr[data["MET"].counts > 0] = (MET < 40)
+        return ret_arr
+
+    def met_40_to_70(self, data):
+        ret_arr = np.full(data.size, False)
+        MET = data["MET"].pt.flatten()
+        ret_arr[data["MET"].counts > 0] = ((MET > 40) & (MET < 70))
+        return ret_arr
+
+    def met_70_to_100(self, data):
+        ret_arr = np.full(data.size, False)
+        MET = data["MET"].pt.flatten()
+        ret_arr[data["MET"].counts > 0] = ((MET > 70) & (MET < 100))
+        return ret_arr
+
+    def met_100_to_150(self, data):
+        ret_arr = np.full(data.size, False)
+        MET = data["MET"].pt.flatten()
+        ret_arr[data["MET"].counts > 0] = ((MET > 100) & (MET < 150))
+        return ret_arr
+
+    def met_150_to_inf(self, data):
+        ret_arr = np.full(data.size, False)
+        MET = data["MET"].pt.flatten()
+        ret_arr[data["MET"].counts > 0] = (MET > 150)
+        return ret_arr
+
+    def fill_dy_nums(self, output, weights, ch, z_win, btag,
+                     met_bin, dsname, data=False):
         LO_DY_ds = ["DYJetsToLL_M-10to50_TuneCP5_13TeV-madgraphMLM-pythia8",
                     "DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8"]
         NLO_DY_ds = ["DYJetsToLL_M-10to50_TuneCP5_13TeV-madgraphMLM-pythia8",
                      "DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8"]
-        print(dsname)
+        if data:
+            name = ("N" + ch[0] + "_" + z_win[0] + "_"
+                    + btag[0] + "_" + met_bin[0])
+        else:
+            name = ("Z" + ch[0] + "_" + z_win[0] + "_"
+                    + btag[0] + "_" + met_bin[0])
+        weights = weights[ch[1] & z_win[1] & btag[1] & met_bin[1]]
         if data or dsname in LO_DY_ds:
-            print(output)
             output["LO_DY_numbers"][name] += weights.sum()
             output["LO_DY_errs"][name] += (weights**2).sum()
         if data or dsname in NLO_DY_ds:
             output["NLO_DY_numbers"][name] += weights.sum()
             output["NLO_DY_errs"][name] += (weights**2).sum()
+
+
+class SFEquations():
+    def __init__(self, met_bins):
+        self.regions = ["in_0b_", "out_0b_", "in_1b_"]
+        self.MET_bins = met_bins
+        data_MC_chs = ["Nee_", "Nem_", "Nmm_", "Zee_", "Zmm_"]
+        self.vals_to_sub = [ch + reg + MET for ch in data_MC_chs
+                            for reg in self.regions for MET in MET_bins]
+        Nee_inc = ""
+        Nmm_inc = ""
+        for reg in self.regions:
+            for MET in MET_bins:
+                Nee_inc += " + Nee_" + reg + MET
+                Nmm_inc += " + Nmm_" + reg + MET
+        Nee_inc = sympy.sympify(Nee_inc)
+        Nmm_inc = sympy.sympify(Nmm_inc)
+        kee = (Nee_inc / Nmm_inc) ** 0.5
+        self.ee_SFs = {}
+        self.em_SFs = {}
+        self.mm_SFs = {}
+        for MET in met_bins:
+            Nee_0b_in = sympy.symbols("Nee_in_0b_" + MET)
+            Nem_0b_in = sympy.symbols("Nem_in_0b_" + MET)
+            Nmm_0b_in = sympy.symbols("Nmm_in_0b_" + MET)
+            Nee_0b_out = sympy.symbols("Nee_out_0b_" + MET)
+            Nem_0b_out = sympy.symbols("Nem_out_0b_" + MET)
+            Nmm_0b_out = sympy.symbols("Nmm_out_0b_" + MET)
+            Nee_1b_in = sympy.symbols("Nee_in_1b_" + MET)
+            Nem_1b_in = sympy.symbols("Nem_in_1b_" + MET)
+            Nmm_1b_in = sympy.symbols("Nmm_in_1b_" + MET)
+
+            Zee_0b_in = sympy.symbols("Zee_in_0b_" + MET)
+            Zmm_0b_in = sympy.symbols("Zmm_in_0b_" + MET)
+            Zee_0b_out = sympy.symbols("Zee_out_0b_" + MET)
+            Zmm_0b_out = sympy.symbols("Zmm_out_0b_" + MET)
+            Zee_1b_in = sympy.symbols("Zee_in_1b_" + MET)
+            Zmm_1b_in = sympy.symbols("Zmm_in_1b_" + MET)
+
+            Ree_0b_data = ((Nee_0b_in - 0.5 * kee * Nem_0b_in)
+                           / (Nee_0b_out - 0.5 * kee * Nem_0b_out))
+            Ree_0b_MC = Zee_0b_in / Zee_0b_out
+            Rmm_0b_data = ((Nmm_0b_in - 0.5 * Nem_0b_in / kee)
+                           / (Nmm_0b_out - 0.5 * Nem_0b_out / kee))
+            Rmm_0b_MC = Zmm_0b_in / Zmm_0b_out
+            self.ee_SFs[MET] = ((Nee_1b_in - 0.5 * kee * Nem_1b_in) / Zee_1b_in
+                                * Ree_0b_MC / Ree_0b_data)
+            self.mm_SFs[MET] = ((Nmm_1b_in - 0.5 * Nem_1b_in / kee) / Zmm_1b_in
+                                * Rmm_0b_MC / Rmm_0b_data)
+            self.em_SFs[MET] = (self.ee_SFs[MET] * self.mm_SFs[MET]) ** 0.5
+
+    def evaluate(self, values):
+        out_dict = {}
+        subs = {val: values[val] for val in self.vals_to_sub}
+        for MET in self.MET_bins:
+            out_dict["is_ee" + MET] = sympy.lambdify(list(subs.keys()),
+                                                     self.ee_SFs[MET])(**subs)
+            out_dict["is_em" + MET] = sympy.lambdify(list(subs.keys()),
+                                                     self.em_SFs[MET])(**subs)
+            out_dict["is_mm" + MET] = sympy.lambdify(list(subs.keys()),
+                                                     self.mm_SFs[MET])(**subs)
+        return out_dict
+
+    def calculate_errs(self):
+        self.ee_SF_errs = {MET: 0 for MET in self.MET_bins}
+        self.em_SF_errs = {MET: 0 for MET in self.MET_bins}
+        self.mm_SF_errs = {MET: 0 for MET in self.MET_bins}
+        err_dicts = [(self.ee_SF_errs, self.ee_SFs),
+                     (self.em_SF_errs, self.em_SFs),
+                     (self.mm_SF_errs, self.mm_SFs)]
+        for MET in self.MET_bins:
+            for err_dict, SFs in err_dicts:
+                for val in self.vals_to_sub:
+                    err_dict[MET] += ((sympy.diff(SFs[MET], val)) ** 2
+                                      * sympy.symbols(val+"_err"))
+                err_dict[MET] = err_dict[MET] ** 0.5
+
+    def evaluate_errs(self, values, errs):
+        out_dict = {}
+        subs = {val: values[val] for val in self.vals_to_sub}
+        subs.update({val + "_err": errs[val] for val in self.vals_to_sub})
+        for MET in self.MET_bins:
+            out_dict["is_ee" + MET] = sympy.lambdify(
+                list(subs.keys()), self.ee_SF_errs[MET])(**subs)
+            out_dict["is_em" + MET] = sympy.lambdify(
+                list(subs.keys()), self.em_SF_errs[MET])(**subs)
+            out_dict["is_mm" + MET] = sympy.lambdify(
+                list(subs.keys()), self.mm_SF_errs[MET])(**subs)
+        return out_dict
 
 
 parser = ArgumentParser(description="Select events from nanoAODs")
@@ -162,7 +311,11 @@ parser.add_argument(
     "and condor_configs to be used with parsl. If not specified, the default "
     "settings in misc.py will be used")
 parser.add_argument(
-    "--DY", action="store_true", help="Run on the processor for the DY CR")
+    "-r", "--rerun", action="store_true", help="Rerun over all the datasets: "
+    "otherwise will just load numbers in from json")
+parser.add_argument(
+    "-t", "--test", action="store_true", help="Run over all histograms and "
+    "produce plots inside and outside the z window")
 args = parser.parse_args()
 
 logger = logging.getLogger("pepper")
@@ -186,8 +339,11 @@ if args.dataset is None:
     DY_ds = ["DYJetsToLL_M-10to50_TuneCP5_13TeV-madgraphMLM-pythia8",
              "DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8",
              "DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8"]
-    mc_ds = {key: val for key, val in config["mc_datasets"].items()
-             if key in DY_ds}
+    if args.test:
+        mc_ds = config["mc_datasets"]
+    else:
+        mc_ds = {key: val for key, val in config["mc_datasets"].items()
+                 if key in DY_ds}
     datasets.update(mc_ds)
 else:
     datasets = {}
@@ -224,8 +380,35 @@ if len(datasets) == 0:
     print("No datasets found")
     exit(1)
 
-# Plotting hists is pointless since we're not running over the full mc:
-config["hists"] = {}
+if args.test:
+    for hist in config["hists"].values():
+        if hasattr(hist, "cats"):
+            hist.cats.extend([coffea.hist.Cat("Z_window", "Z_window"),
+                              coffea.hist.Cat("MET_bin", "MET_bin"),
+                              coffea.hist.Cat("At least 1 btag",
+                                              "At least 1 btag")])
+            hist.cat_fill_methods["Z_window"] = {"in": ["Z window",
+                                                        {"function": "not"}],
+                                                 "out": ["Z window"]}
+            hist.cat_fill_methods["MET_bin"] = {MET_bin: [MET_bin]
+                                                for MET_bin in MET_bins}
+            hist.cat_fill_methods["At least 1 btag"] = \
+                {"0b": ["At least 1 btag", {"function": "not"}],
+                 "1b": ["At least 1 btag"]}
+        else:
+            hist.cats = [coffea.hist.Cat("Z_window", "Z_window"),
+                         coffea.hist.Cat("MET_bin", "MET_bin"),
+                         coffea.hist.Cat("At least 1 btag", "At least 1 btag")]
+            hist.cat_fill_methods = \
+                {"Z_window": {"in": ["Z window"],
+                              "out": ["Z window", {"function": "not"}]},
+                 "MET_bin": {MET_bin: [MET_bin] for MET_bin in MET_bins},
+                 "At least 1 btag": {"0b": ["At least 1 btag",
+                                            {"function": "not"}],
+                                     "1b": ["At least 1 btag"]}}
+else:
+    # Plotting hists is pointless if we're not running over the full mc:
+    config["hists"] = {}
 
 processor = DYprocessor(config, args.eventdir)
 
@@ -251,39 +434,59 @@ else:
     executor = coffea.processor.iterative_executor
     executor_args = {}
 
-output = coffea.processor.run_uproot_job(
-    datasets, "Events", processor, executor, executor_args,
-    chunksize=args.chunksize)
+if args.rerun or args.test:
+    output = coffea.processor.run_uproot_job(
+        datasets, "Events", processor, executor, executor_args,
+        chunksize=args.chunksize)
+else:
+    with open("DY_sfs.json", "r") as f:
+        output = json.load(f)
 
+print(output)
 nums = {"LO_DY_numbers": output["LO_DY_numbers"],
         "LO_DY_errs": output["LO_DY_errs"],
         "LO_SFs": {},
+        "LO_SF_errs": {},
         "NLO_DY_numbers": output["NLO_DY_numbers"],
         "NLO_DY_errs": output["NLO_DY_errs"],
-        "NLO_SFs": {}}
+        "NLO_SFs": {},
+        "NLO_SF_errs": {}}
 
-Nee_in = output["LO_DY_numbers"]["Nee_in"]
-Nem_in = output["LO_DY_numbers"]["Nem_in"]
-Nmm_in = output["LO_DY_numbers"]["Nmm_in"]
-Zee_in = output["LO_DY_numbers"]["Zee_in"]
-Zmm_in = output["LO_DY_numbers"]["Zmm_in"]
-kee = np.sqrt(Nee_in/Nmm_in)
+sf_eq = SFEquations(MET_bins)
+nums["LO_SFs"] = sf_eq.evaluate(output["LO_DY_numbers"])
+nums["NLO_SFs"] = sf_eq.evaluate(output["NLO_DY_numbers"])
+sf_eq.calculate_errs()
+nums["LO_SF_errs"] = sf_eq.evaluate_errs(output["LO_DY_numbers"],
+                                         output["LO_DY_errs"])
+nums["NLO_SF_errs"] = sf_eq.evaluate_errs(output["NLO_DY_numbers"],
+                                          output["NLO_DY_errs"])
 
-nums["LO_SFs"]["is_ee"] = (Nee_in - 0.5 * kee * Nem_in) / Zee_in
-nums["LO_SFs"]["is_mm"] = (Nmm_in - 0.5 * Nem_in / kee) / Zmm_in
-nums["LO_SFs"]["is_em"] = np.sqrt(nums["LO_SFs"]["is_ee"]
-                                  * nums["LO_SFs"]["is_mm"])
-
-Nee_in = output["NLO_DY_numbers"]["Nee_in"]
-Nem_in = output["NLO_DY_numbers"]["Nem_in"]
-Nmm_in = output["NLO_DY_numbers"]["Nmm_in"]
-Zee_in = output["NLO_DY_numbers"]["Zee_in"]
-Zmm_in = output["NLO_DY_numbers"]["Zmm_in"]
-kee = np.sqrt(Nee_in/Nmm_in)
-
-nums["NLO_SFs"]["is_ee"] = (Nee_in - 0.5 * kee * Nem_in) / Zee_in
-nums["NLO_SFs"]["is_mm"] = (Nmm_in - 0.5 * Nem_in / kee) / Zmm_in
-nums["NLO_SFs"]["is_em"] = np.sqrt(nums["NLO_SFs"]["is_ee"]
-                                   * nums["NLO_SFs"]["is_mm"])
 with open("DY_sfs.json", "w") as f:
     json.dump(nums, f, indent=4)
+
+if args.test:
+    # Save histograms with a hist.json describing the hist files
+    hists = output["hists"]
+    jsonname = "hists.json"
+    hists_forjson = {}
+    for key, hist in hists.items():
+        if hist.values() == {}:
+            continue
+        cuts = next(iter(output["cutflows"]["all"].values())).keys()
+        cutnum = list(cuts).index(key[0])
+        fname = "Cut {:03} {}.coffea".format(cutnum, "_".join(key))
+        fname = fname.replace("/", "")
+        coffea.util.save(hist, os.path.join(args.histdir, fname))
+        hists_forjson[key] = fname
+    with open(os.path.join(args.histdir, jsonname), "a+") as f:
+        try:
+            hists_injson = {tuple(k): v for k, v in zip(*json.load(f))}
+        except json.decoder.JSONDecodeError:
+            hists_injson = {}
+    hists_injson.update(hists_forjson)
+    with open(os.path.join(args.histdir, jsonname), "w") as f:
+        json.dump([[tuple(k) for k in hists_injson.keys()],
+                   list(hists_injson.values())], f, indent=4)
+
+    # Save cutflows
+    coffea.util.save(output["cutflows"], "cutflows.coffea")
